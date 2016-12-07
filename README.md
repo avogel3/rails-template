6da51ea481c08a18413d6a75e6117b06eee0ee3f
Andrew Vogel <andrew.vogel@elvamarketing.com>
2016-12-06 22:50:08 -0500

    Add gem_groups and rails commands.

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 ### Rails App Template
 
+#### Information
+
+Takes some of the configuration out of building a rails app. It only includes the barebones gems for setup.
+
+* Sets up Devise as the default for authorization, also adds the devise model User
+* Sets up RSpec as the test framework
+* Runs bundle install
+* Runs db:create and db:migrate
+* Finally, adds the initial commit.
+
 #### Usage
 
-```$ 
-rails new APP_NAME -m ~/template.rb
+```bash
+rails new APP_NAME -m ~/template.rb --database=postgresql
 ```
 
 ###### Andrew Vogel
---
+++ template.rb
@@ -0,0 +1,34 @@
+gem "haml-rails"
+gem "devise"
+
+gem_group :test do
+  gem "simplecov"
+end
+
+gem_group :development, :test do
+  gem "rspec-rails"
+  gem "faker"
+  gem "factory_girl_rails"
+  gem "pry"
+  gem "capybara"
+end
+
+gem_group :development do
+  gem "spring-commands-rspec" # Speed up Tests in development
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem "hirb"
+end
+
+run "bundle install"
+rails_command("db:create")
+run "rails g rspec:install"
+run "rails g devise:install"
+environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: "development"
+run "rails g devise User"
+rails_command("db:migrate")
+
+git :init
+git add: "."
+git commit: "-a -m 'Initial commit'"
+
