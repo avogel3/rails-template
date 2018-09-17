960f1b849b539e8193c0d3781792525eb58b5b76
Andrew Vogel <andrewpvogel@gmail.com>
2018-09-17 10:40:25 -0400

    Update commit message

--- template.rb
+++ template.rb
@@ -15,11 +15,15 @@ end
 
 run 'bundle install'
 rails_command('db:create')
+
 run 'rails g rspec:install'
+
+# Install devise and create User
 run 'rails g devise:install'
 environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
 run 'rails g devise User'
+
 rails_command('db:migrate')
 
 git add: '.'
-git commit: "-m 'Generate with template from github.com/avogel3/rails-template'"
+git commit: "-m 'Generate project with template - github.com/avogel3/rails-template'"
